b6a0b190bad7a23f60b28afa7af49c14bf4646c7
User <asylzatwww@yandex.ru>
2019-05-21 14:18:58 +0300

    Class on instagram changed, simplified, fixed bugs

--- logBookGetMap.js
+++ logBookGetMap.js
@@ -9,66 +9,56 @@ var Location = "";
 
 var mapUrl = "https://www.google.com/maps/dir/";
 var dataClass;
-if ($('#eld-ev-s-text-content')[0]) dataClass = $('#eld-ev-s-text-content > tr');
-else dataClass = $('#eld-ev-o-text-content  > tr');
+if ($("#eld-ev-s-text-content")[0]) dataClass = $("#eld-ev-s-text-content > tr");
+else dataClass = $("#eld-ev-o-text-content  > tr");
 
 var count = $( dataClass ).length;
-// $('#eld-ev-o-text-content  > tr').find('td:nth-child(2)').text().trim();
 $( dataClass ).each(function(i) {
 
-    /*console.log( $(this).find('td:nth-child(1)').text().trim() );
-    console.log( $(this).find('td:nth-child(2)').text().trim() );
-    console.log( $(this).find('td:nth-child(3)').text().trim() );
-    console.log( $(this).find('td:nth-child(4)').text().trim() );
-    console.log( $(this).find('td:nth-child(5)').text().trim() );
+    var ind = $(this).find("td:nth-child(1)").text().trim();
+    var loc = $(this).find("td:nth-child(5)").text().trim();
+    var timeStart = $(this).find("td:nth-child(3)").text().trim();
+    var status = $(this).find("td:nth-child(2)").text().trim();
+    var notes = $(this).find("td:nth-child(8)").text().trim();
 
-    console.log( $(this).find('td:nth-child(5)').text().trim().slice( ($(this).find('td:nth-child(5)').text().trim().indexOf(' of ') ) + 4) );
-
-    console.log( $(this).find('td:nth-child(8)').text().trim() );*/
-
-    var ind = $(this).find('td:nth-child(1)').text().trim();
-    var loc = $(this).find('td:nth-child(5)').text().trim();
-    var timeStart = $(this).find('td:nth-child(3)').text().trim();
-    var status = $(this).find('td:nth-child(2)').text().trim();
-    var notes = $(this).find('td:nth-child(8)').text().trim();
-
-    if (notes == '') notes = " ";
+    if (notes == "") notes = " ";
     moveHistory[ ind ] = [];
 
-    moveHistory[ ind ]['loc'] = loc;
-    moveHistory[ ind ]['timeStart'] = timeStart;
-    moveHistory[ ind ]['status'] = status;
+    moveHistory[ ind ]["loc"] = loc;
+    moveHistory[ ind ]["timeStart"] = timeStart;
+    moveHistory[ ind ]["status"] = status;
 
     moveHistoryStr += "moveHistory[" + ind + "] = [];";
 
-    moveHistoryStr += "moveHistory[" + ind + "]['loc'] = '" + loc + "';";
-    moveHistoryStr += "moveHistory[" + ind + "]['timeStart'] = '" + timeStart + "';";
-    moveHistoryStr += "moveHistory[" + ind + "]['status'] = '" + status + "';";
-    moveHistoryStr += "moveHistory[" + ind + "]['notes'] = '" + notes + "';";
-    var statusBtn = '';
+    moveHistoryStr += "moveHistory[" + ind + "][\"loc\"] = \"" + loc + "\";";
+    moveHistoryStr += "moveHistory[" + ind + "][\"timeStart\"] = \"" + timeStart + "\";";
+    moveHistoryStr += "moveHistory[" + ind + "][\"status\"] = \"" + status + "\";";
+    moveHistoryStr += "moveHistory[" + ind + "][\"notes\"] = \"" + notes + "\";";
+    var statusBtn = "";
     switch (status) {
-        case 'Off Duty' : statusBtn = 'edit-event-set-off-duty';break;
-        case 'SB' : statusBtn = 'edit-event-set-sleeper';break;
-        case 'Driving' : statusBtn = 'edit-event-set-driving';break;
-        case 'ddd' : statusBtn = 'edit-event-set-ym';break;
-        case 'ddd' : statusBtn = 'edit-event-set-pc';break;
-        case 'On Duty' : statusBtn = 'edit-event-set-on-duty';break;
+        case "Off Duty" : statusBtn = "edit-event-set-off-duty";break;
+        case "SB" : statusBtn = "edit-event-set-sleeper";break;
+        case "Driving" : statusBtn = "edit-event-set-driving";break;
+        case "ddd" : statusBtn = "edit-event-set-ym";break;
+        case "ddd" : statusBtn = "edit-event-set-pc";break;
+        case "On Duty" : statusBtn = "edit-event-set-on-duty";break;
     }
-    moveHistoryStr += "moveHistory[" + ind + "]['statusBtn'] = '" + statusBtn + "';";
-    firstLoc = $(this).find('td:nth-child(5)').text().trim().slice( ($(this).find('td:nth-child(5)').text().trim().indexOf(' of ') ) + 4);
+    moveHistoryStr += "moveHistory[" + ind + "][\"statusBtn\"] = \"" + statusBtn + "\";";
+    firstLoc = $(this).find("td:nth-child(5)").text().trim().slice( ($(this).find("td:nth-child(5)").text().trim().indexOf(" of ") ) + 4);
 
     if (firstLoc != Location){
-        mapUrl += firstLoc.replace(' ', '+').replace(' ', '+').replace(' ', '+')
-            .replace(' ', '+') + ',+USA/';
+        mapUrl += firstLoc.replace(" ", "+").replace(" ", "+").replace(" ", "+")
+            .replace(" ", "+") + ",+USA/";
 
-        //'Addison+City,+IL,+USA/Lake+Havasu+City,+Arizona,+USA/Gallup,+New+Mexico/@?hl=en';
         Location = firstLoc;
     }
 
     if (i+1 === count) {
-        //console.log( moveHistoryStr );
-        //copy( moveHistoryStr );
-        console.log( mapUrl + '@?hl=en' );
+
+        mapUrl += "@?hl=en";
+        console.log( mapUrl );
+
+        window.open(mapUrl, "_blank")
     }
 
 });
